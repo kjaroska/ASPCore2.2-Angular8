fd338a80c7b2eb577fb13acbd2f019a7e33fcde4
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-23 18:04:31 +0200

    Add validation via DataAnnotations to UserDto

--- DatingApp.API/Models/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/Models/DTOs/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Models.DTOs
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(16, MinimumLength = 4, ErrorMessage ="Password too long, min 4 characters.")]
         public string Password { get; set; }
     }
 }
