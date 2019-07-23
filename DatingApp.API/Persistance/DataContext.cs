15fbb2a30997c666c4b792d6e0e005c795f88b61
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-23 17:20:13 +0200

    Add User Model, ef migration and db update

--- DatingApp.API/Persistance/DataContext.cs
+++ DatingApp.API/Persistance/DataContext.cs
@@ -6,6 +6,7 @@ namespace DatingApp.API.Persistance
     public class DataContext : DbContext
     {
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
     }
