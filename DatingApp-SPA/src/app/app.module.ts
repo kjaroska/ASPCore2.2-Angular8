b39239d948f015857be45469fac37b63026412e3
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-26 16:21:41 +0200

    Generate Home and Register components

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
@@ -20,7 +24,7 @@ import { NavComponent } from './nav/nav.component';
       FormsModule
    ],
    providers: [
-     AuthService
+      AuthService
    ],
    bootstrap: [
       AppComponent
