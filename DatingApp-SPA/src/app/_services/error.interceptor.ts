221f1aeb7064e1d8ea7c4478f336d13dbb67c3bd
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-29 15:51:18 +0200

    Handle 400, 401 and 500 errors via Interceptor

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -9,12 +9,28 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req:HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError(error => {
+        if (error.status === 401) {
+          return throwError(error.statusText);
+        }
+
         if(error instanceof HttpErrorResponse) {
-          const applicationError = error.headers.get("Application-Error");
+          const applicationError = error.headers.get("Application-Error"); // 400
           if (applicationError) {
             console.error(applicationError);
             return throwError(applicationError);
           }
+
+          const serverError = error.error.errors; // 500
+          let modalStateErrors = "";
+          if(serverError && typeof serverError === "object") {
+            for (const key in serverError) {
+              if (serverError[key]) {
+                modalStateErrors += serverError[key] + "\n";
+              }
+            }
+          }
+
+          return throwError(modalStateErrors || serverError || "Server Error");
         }
       })
     );
