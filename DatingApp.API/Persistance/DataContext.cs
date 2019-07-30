7ac9a378c9029e7830deb7d480599263b37b02cf
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-30 11:15:10 +0200

    Add Photos to DataContext

--- DatingApp.API/Persistance/DataContext.cs
+++ DatingApp.API/Persistance/DataContext.cs
@@ -7,6 +7,7 @@ namespace DatingApp.API.Persistance
     {
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
     }
