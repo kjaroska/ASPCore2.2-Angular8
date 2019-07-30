2b8ca3908e8525d3c3c293c037611382d8d30d62
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-30 10:41:21 +0200

    Add redirect after login/logout

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,26 +1,36 @@
-import { AlertifyService } from './../_services/alertify.service';
-import { AuthService } from './../_services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from "./../_services/alertify.service";
+import { AuthService } from "./../_services/auth.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      this.alertify.success("Logged Successfuly");
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success("Logged Successfuly");
+      },
+      error => {
+        this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(["/members"]);
+      }
+    );
   }
 
   loggedIn() {
@@ -30,5 +40,6 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem("token");
     this.alertify.message("Logged Out");
+    this.router.navigate(["home"]);
   }
 }
