fde813b0c68dd07918785a4554d40c11fab20f11
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-30 10:37:31 +0200

    Implement Routes for Navigation Bar

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,12 +2,13 @@ import { MessagesComponent } from "./messages/messages.component";
 import { MemberListComponent } from "./member-list/member-list.component";
 import { HomeComponent } from "./home/home.component";
 import { Routes } from "@angular/router";
+import { ListsComponent } from './lists/lists.component';
 
 export const appRoutes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "members", component: MemberListComponent },
   { path: "messages", component: MessagesComponent },
-  { path: "lists", component: HomeComponent },
+  { path: "lists", component: ListsComponent },
   { path: "home", component: HomeComponent },
   { path: "**", redirectTo: "home", pathMatch: "full" }
 ];
