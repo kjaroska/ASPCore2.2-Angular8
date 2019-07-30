cb783e4faab197da5b6c71cd55332b2040c1ff9e
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-30 13:52:48 +0200

    Comment out the SeedUsers(), can be used at any point in the future to restore the data

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -72,7 +72,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            seeder.SeedUsers();
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
