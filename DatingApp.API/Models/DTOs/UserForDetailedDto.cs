d6191d76588f8b2542790c24f160b36237454ce0
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-31 16:04:51 +0200

    Fix Typo in Models

--- DatingApp.API/Models/DTOs/UserForDetailedDto.cs
+++ DatingApp.API/Models/DTOs/UserForDetailedDto.cs
@@ -9,7 +9,7 @@ namespace DatingApp.API.Models.DTOs
         public string Username { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
-        public string KnowsAs { get; set; }
+        public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Introduction { get; set; }
