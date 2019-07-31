6b84565a5c341e76e65d21677061196fce28ee84
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-07-31 12:02:28 +0200

    Fix missing parameter in GetUser()

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -24,7 +24,7 @@ namespace DatingApp.API.Controllers
             return Ok(users);
         }
 
-        [HttpGet]
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
