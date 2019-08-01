f1cf6f3d15cc8ba6ca4c6d35647ae75fa7891687
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-08-01 13:00:15 +0200

    Import ngx tabset, Implement tabs in member-detail component, add general styles

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-import { BsDropdownModule } from "ngx-bootstrap";
+import { BsDropdownModule, TabsModule } from "ngx-bootstrap";
 import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from "./app.component";
@@ -44,6 +44,7 @@ export function tokenGetter() {
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     JwtModule.forRoot({
       config: {
