a9f4e07568888be29bda4f07af9b4bd8a4a09e79
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-08-01 11:48:52 +0200

    BugFix, remove reduntant mapping from profile

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -15,9 +15,6 @@ namespace DatingApp.API.Helpers
                         src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
             CreateMap<User, UserForDetailedDto>()
-                .ForMember(dest => dest.Photos, opt =>
-                    opt.MapFrom(src => 
-                        src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
