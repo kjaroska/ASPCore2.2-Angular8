cad5ac462957283138e66ffa3d44adff1bcfcfe9
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-08-07 16:10:03 +0200

    Implement POC for PhotoController with Cloudinary

--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -11,5 +11,6 @@ namespace DatingApp.API.Models
         public bool IsMain { get; set; }
         public User User { get; set; }
         public int UserId { get; set; }
+        public string PublicId { get; set; }
     }
 }
