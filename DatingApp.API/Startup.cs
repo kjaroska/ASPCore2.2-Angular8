73173e47be2f38a373dcc97c0eaaf119296bbafa
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-08-07 16:00:00 +0200

    Introduce Cloudinary

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -32,6 +32,7 @@ namespace DatingApp.API
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddTransient<Seed>();
