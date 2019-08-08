2d14c89e52500a834f0a70ea02adfb71ae7b1f5d
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-08-08 14:37:11 +0200

    Implement GetMainPhoto in PhotosController

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -10,50 +10,50 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+  [Authorize]
+  [Route("api/[controller]")]
+  public class UsersController : ControllerBase
+  {
+    private readonly IDatingRepository _repo;
+    private readonly IMapper _mapper;
+
+    public UsersController(IDatingRepository repo, IMapper mapper)
     {
-        private readonly IDatingRepository _repo;
-        private readonly IMapper _mapper;
-
-        public UsersController(IDatingRepository repo, IMapper mapper)
-        {
-            this._repo = repo;
-            this._mapper = mapper;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetUsers()
-        {
-            var users = await _repo.GetUsers();
-            var usersToReturn = _mapper.Map<IEnumerable<UserForListsDto>>(users);
-
-            return Ok(usersToReturn);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser(int id)
-        {
-            var user = await _repo.GetUser(id);
-            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
-
-            return Ok(userToReturn);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateUser(int id, [FromBody] UserForUpdateDto userForUpdate)
-        {
-            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
-                return Unauthorized();
-
-            var userFromRepo = await _repo.GetUser(id);
-            _mapper.Map(userForUpdate, userFromRepo);
-
-            if (await _repo.SaveAll())
-                return NoContent();
-
-            throw new Exception($"Updating user {id} failed on save!");
-        }
+      this._repo = repo;
+      this._mapper = mapper;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+      var users = await _repo.GetUsers();
+      var usersToReturn = _mapper.Map<IEnumerable<UserForListsDto>>(users);
+
+      return Ok(usersToReturn);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+      var user = await _repo.GetUser(id);
+      var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+
+      return Ok(userToReturn);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] UserForUpdateDto userForUpdate)
+    {
+      if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+        return Unauthorized();
+
+      var userFromRepo = await _repo.GetUser(id);
+      _mapper.Map(userForUpdate, userFromRepo);
+
+      if (await _repo.SaveAll())
+        return NoContent();
+
+      throw new Exception($"Updating user {id} failed on save!");
+    }
+  }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -6,21 +6,21 @@ using DatingApp.API.Models.DTOs;
 
 namespace DatingApp.API.Helpers
 {
-    public class AutoMapperProfiles : Profile
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
     {
-        public AutoMapperProfiles()
-        {
-            CreateMap<User, UserForListsDto>()
-                .ForMember(dest => dest.PhotoUrl, opt =>
-                    opt.MapFrom(src =>
-                        src.Photos.FirstOrDefault(p => p.IsMain).Url))
-                .ForMember(dest => dest.Age, opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
-            CreateMap<User, UserForDetailedDto>()
-                .ForMember(dest => dest.Age, opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
-            CreateMap<Photo, PhotosForDetailedDto>();
-            CreateMap<UserForUpdateDto, User>();
-            CreateMap<Photo, PhotoForReturnDto>();
-            CreateMap<PhotoForCreationDto, Photo>();
-        }
+      CreateMap<User, UserForListsDto>()
+          .ForMember(dest => dest.PhotoUrl, opt =>
+              opt.MapFrom(src =>
+                  src.Photos.FirstOrDefault(p => p.IsMain).Url))
+          .ForMember(dest => dest.Age, opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
+      CreateMap<User, UserForDetailedDto>()
+          .ForMember(dest => dest.Age, opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()));
+      CreateMap<Photo, PhotosForDetailedDto>();
+      CreateMap<UserForUpdateDto, User>();
+      CreateMap<Photo, PhotoForReturnDto>();
+      CreateMap<PhotoForCreationDto, Photo>();
     }
+  }
 }
