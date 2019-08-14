3885ed9bfac05d36fac086c5b70f9a4f5781c5e5
Krzysztof <kjaroska@gmail.com>
2019-08-14 15:37:56 +0200

    Extend UserForRegister model and add MappingProfile

--- DatingApp.API/Models/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/Models/DTOs/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Models.DTOs
@@ -7,7 +8,33 @@ namespace DatingApp.API.Models.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
-        [StringLength(16, MinimumLength = 4, ErrorMessage ="Password too long, min 4 characters.")]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "Password too long, min 4 characters.")]
+
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownsAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
