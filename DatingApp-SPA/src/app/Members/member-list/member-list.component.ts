0a80ce53ab13477b81c57db35cb1afabe9ce0461
Krzysztof D. Jaroska <kjaroska@gmail.com>
2019-08-22 15:55:37 +0200

    Implement loadUsers() with new Pagination feature

--- DatingApp-SPA/src/app/Members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/Members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { PaginatedResult } from './../../_models/pagination';
 import { ActivatedRoute } from "@angular/router";
 import { AlertifyService } from "../../_services/alertify.service";
 import { UserService } from "../../_services/user.service";
@@ -29,14 +30,16 @@ export class MemberListComponent implements OnInit {
 
   pageChanged(event: any): void {
     this.pagination.currentPage = event.page;
-    console.log(this.pagination.currentPage)
+    this.loadUsers();
   }
 
-  // loadUsers() {
-  //   this.userService.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+    .subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 }
